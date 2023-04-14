c3eebef19e8b7e554db3436ba0d9f45b0bface4b
TunaErkmen <98965068+TunaErkmen@users.noreply.github.com>
2023-04-14 18:10:18 +0200

    uploaded cloudinary configuration

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const fileUploader = require("../config/cloudinary.config");
 const Post = require("../models/Post.model");
 
 router.post("/posts", (req, res, next) => {
@@ -34,6 +34,21 @@ router.get("/posts", (req, res, next) => {
             });
         })
 })
+
+// POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
+router.post("/upload", fileUploader.single("image"), (req, res, next) => {
+    // console.log("file is: ", req.file)
+   
+    if (!req.file) {
+      next(new Error("No file uploaded!"));
+      return;
+    }
+    
+    // Get the URL of the uploaded file and send it as a response.
+    // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
+    
+    res.json({ fileUrl: req.file.path });
+  });
 
 router.delete("/posts/:postId", (req, res, next ) => {
     const { postId } = req.params;
